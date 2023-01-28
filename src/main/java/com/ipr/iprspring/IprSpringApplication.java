57ad4559aac781658772c2f77b8a7ebf9cf0de34
Ашот агабеков <agabekov.aa@esphere.ru>
2023-01-28 14:46:23 +0300

    commit

---
+++ src/main/java/com/ipr/iprspring/IprSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipr.iprspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IprSpringApplication.class, args);
+    }
+
+}
